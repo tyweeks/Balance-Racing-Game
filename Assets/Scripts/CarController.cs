65a18249d2d73283366471552dbb17d5a26126e9
Ty Weeks <ty.weeks10@gmail.com>
2022-10-11 11:31:17 -0700

    Add front wheel motor.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,6 +6,7 @@ public class CarController : MonoBehaviour
 {
     public float speed = 1500;
     public WheelJoint2D backWheel;
+    public WheelJoint2D frontWheel;
 
     private float movement = 0f;
 
@@ -19,12 +20,16 @@ public class CarController : MonoBehaviour
         if (movement == 0)
         {
             backWheel.useMotor = false;
+            frontWheel.useMotor = false;
         }
         else
         {
             backWheel.useMotor = true;
+            frontWheel.useMotor = true;
+
             JointMotor2D motor = new JointMotor2D { motorSpeed = movement, maxMotorTorque = backWheel.motor.maxMotorTorque };
             backWheel.motor = motor;
+            frontWheel.motor = motor;
         }
     }
 }
